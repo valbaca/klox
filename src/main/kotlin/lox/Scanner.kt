3075898bc1d157e939c48d8ba37d261906d96dc9
Valentin Baca <valbaca@gmail.com>
2022-04-30 16:44:23 -0700

    End of Ch 4

--- src/main/kotlin/lox/Scanner.kt
+++ src/main/kotlin/lox/Scanner.kt
@@ -1,5 +1,9 @@
 package lox
 
+import lox.ScannerConstants.ALPHAS
+import lox.ScannerConstants.DIGITS
+import lox.ScannerConstants.KEYWORDS
+import lox.ScannerConstants.NUL
 import lox.TokenType.*
 
 class Scanner(val source: String) {
@@ -48,7 +52,12 @@ class Scanner(val source: String) {
             ' ', '\r', '\t' -> {} // ignored whitespace
             '\n' -> line++
             '"' -> string()
-            else -> error(line, "Unexpected character.")
+            // 'else' is Kotlin equivalent of default:
+            else -> when {
+                isDigit(c) -> number()
+                isAlpha(c) -> identifier()
+                else -> error(line, "Unexpected character.")
+            }
         }
     }
 
@@ -68,12 +77,12 @@ class Scanner(val source: String) {
 
     // Like advance, but doesn't consume the character
     private fun peek(): Char {
-        if (isAtEnd()) return '\u0000'
+        if (isAtEnd()) return NUL
         return source[current]
     }
 
     private fun string() {
-        while (peek() != '"' && !isAtEnd()){
+        while (peek() != '"' && !isAtEnd()) {
             if (peek() == '\n') line++
             advance()
         }
@@ -87,4 +96,54 @@ class Scanner(val source: String) {
         val value = source.substring(start + 1, current - 1)
         addToken(STRING, value)
     }
+
+    private fun isDigit(c: Char): Boolean = c in DIGITS
+    private fun number() {
+        while (isDigit(peek())) advance()
+        if (peek() == '.' && isDigit(peekNext())) {
+            // consume the "."
+            advance()
+            while (isDigit(peek())) advance()
+        }
+        addToken(NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun peekNext(): Char {
+        if (current + 1 >= source.length) return NUL
+        return source[current + 1]
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+        val text = source.substring(start, current)
+        val type = KEYWORDS[text]?: IDENTIFIER
+        addToken(type)
+    }
+    private fun isAlpha(c: Char): Boolean = c in ALPHAS
+    private fun isAlphaNumeric(c: Char) = isAlpha(c) || isDigit(c)
+}
+
+object ScannerConstants {
+    const val NUL = '\u0000' // Weirdly Kotlin doesn't support '\0' as a char o.O
+    val DIGITS = '0'..'9'
+    val ALPHAS = ('a'..'z').union('A'..'Z').plus('_')
+    val KEYWORDS = mapOf<String, TokenType>(
+        "and" to AND,
+        "class" to CLASS,
+        "else" to ELSE,
+        "false" to FALSE,
+        "for" to FOR,
+        "fun" to FUN,
+        "if" to IF,
+        "nil" to NIL,
+        "or" to OR,
+        "print" to PRINT,
+        "return" to RETURN,
+        "super" to SUPER,
+        "this" to THIS,
+        "true" to TRUE,
+        "var" to VAR,
+        "while" to WHILE
+
+    )
 }
