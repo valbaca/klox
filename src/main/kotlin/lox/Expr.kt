5fee12304715aa766ae5f344b6fea6257ee93ff2
Valentin Baca <valbaca@gmail.com>
2022-05-01 13:05:15 -0700

    Commit generated Expr.kt

--- src/main/kotlin/lox/Expr.kt
+++ src/main/kotlin/lox/Expr.kt
@@ -1,9 +1,47 @@
 package lox
 
-abstract class Expr
+abstract class Expr {
+    abstract fun <R> accept(visitor: Visitor<R>): R
+    interface Visitor<R> {
+        fun visitExpr(expr: Binary): R
+        fun visitExpr(expr: Grouping): R
+        fun visitExpr(expr: Literal): R
+        fun visitExpr(expr: Unary): R
+    }
+}
 
 class Binary(
     val left: Expr,
     val operator: Token,
     val right: Expr,
-) : Expr()
+) : Expr() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitExpr(this)
+    }
+}
+
+class Grouping(
+    val expression: Expr,
+) : Expr() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitExpr(this)
+    }
+}
+
+class Literal(
+    val value: Any,
+) : Expr() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitExpr(this)
+    }
+}
+
+class Unary(
+    val operator: Token,
+    val right: Expr,
+) : Expr() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitExpr(this)
+    }
+}
+
